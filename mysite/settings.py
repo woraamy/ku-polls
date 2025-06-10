07764fcc181970805375e70dada54ff2cc3fa8b0
Worawalan C. <113490970+woraamy@users.noreply.github.com>
2025-06-10 10:49:47 +0900

    Add setting modules

--- mysite/settings.py
+++ mysite/settings.py
@@ -137,3 +137,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJANGO_SETTINGS_MODULE = 'mysite.settings pytest --cov=. --cov-report=xml'
